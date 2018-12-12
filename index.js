2a47bfbcfb54a615676f75a7923bdf0faa2e9076
Alexander <krasnoyurchenko.a.s@gmail.com>
2018-12-12 12:23:09 +0400

    add `singular` query and replace `json-server` to `json-server-relationship`

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const port = 3000;
 
 server.use(middlewares);
 
-// Add `singular` query
+// If filter on `id` or `slug` query
 server.use((req, res, next) => {
+	const _status = res.status;
 	const _send = res.send;
 
 	res.send = function (body) {
-		if (require('url').parse(req.originalUrl, true).query['singular']) {
+		if (req.query.id !== undefined || req.query.slug !== undefined) {
 			try {
 				const json = JSON.parse(body);
 
@@ -20,10 +21,11 @@ server.use((req, res, next) => {
 					if (json.length === 1) {
 						return _send.call(this, JSON.stringify(json[0], null, 2));
 					} else if (json.length === 0) {
-						return _send.call(this, '{}', 404);
+						return _status.call(this, 404).send('{}');
 					}
 				}
-			} catch (e) {}
+			} catch (e) {
+			}
 		}
 
 		return _send.call(this, body);
